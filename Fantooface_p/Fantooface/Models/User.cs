ff372cbdae6874880fc4096b412ed1ac8896f42c
Shrikant Singh <shrikantsingh108@gmail.com>
2015-10-30 09:05:43 +0530

    change

---
+++ Fantooface_p/Fantooface/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+namespace Fantooface.Models
+{
+    public class User
+    {
+        public int UserId;
+        public string DisplayName;
+        public string UserName;
+    }
+}
---
+++ Fantooface_p/Fantooface/Models/UserManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+namespace Fantooface.Models
+{
+    public class UserManager
+    {
+    }
+}
